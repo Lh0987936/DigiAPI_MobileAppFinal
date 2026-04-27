93dddec6e400a53ee54effac6480152a5c0447b6
Loghoo <lh0987936@otc.edu>
2026-04-27 08:28:36 -0500

    Initial Commit

---
+++ FinalHoover/FinalHooverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalHooverApp.swift
+//  FinalHoover
+//
+//  Created by HOOVER, LOGAN on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalHooverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
